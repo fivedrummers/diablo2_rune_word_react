6fd85836a5e05f7af0e2b1bb524341f871b31aeb
cookiedoug <cookiedoug73901@gmail.com>
2021-12-07 17:40:47 +0800

    list rune function

--- src/runeWordApp.js
+++ src/runeWordApp.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Rune from './rune'
+import Rune, { runeIds } from './rune'
 
 export default function RuneWord () {
+  function handleSubmit () {
+    alert(runeIds)
+  }
+
   return (
     <section>
-      <form>
-        <label>Combine Rune Word</label>
+      <h1>Combine Rune Word</h1>
+      <form onSubmit={handleSubmit}>
         <Rune />
+        <br />
+        <br />
+        <input type='submit' value='提交' />
       </form>
     </section>
   )
